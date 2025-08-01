e036adaed5b29e21878096f599bd38d0f5b62b9e
Marius Pik <marius.pikciunas@gmail.com>
2025-08-01 14:56:33 +0200

    backend change to s3

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,11 @@
 terraform {
-
-  # cloud {
-  #   workspaces {
-  #     name = "learn-terraform-eks"
-  #   }
-  # }
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-mariuspik"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
