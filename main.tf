ad69b9f7d1ebfb7b7cf5f2e7e1b0b367fbc95459
Marius Pik <marius.pikciunas@gmail.com>
2025-08-01 15:57:50 +0200

    destroy eks

--- main.tf
+++ main.tf
@@ -45,83 +45,83 @@ module "vpc" {
   }
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.8.5"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+#   cluster_name    = local.cluster_name
+#   cluster_version = "1.29"
 
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
+#   cluster_endpoint_public_access           = true
+#   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
-  }
+#   cluster_addons = {
+#     aws-ebs-csi-driver = {
+#       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#     }
+#   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+#   eks_managed_node_group_defaults = {
+#     ami_type = "AL2_x86_64"
 
-  }
+#   }
 
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+#       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 2
+#     }
 
-    two = {
-      name = "node-group-2"
+#     two = {
+#       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+#       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
+#       min_size     = 1
+#       max_size     = 2
+#       desired_size = 1
+#     }
+#   }
 
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::563746928741:user/terraform-user"
+#   access_entries = {
+#     # One access entry with a policy associated
+#     example = {
+#       kubernetes_groups = []
+#       principal_arn     = "arn:aws:iam::563746928741:user/terraform-user"
 
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type       = "cluster"
-          }
-        }
-      }
-    }
-  }
-}
+#       policy_associations = {
+#         example = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#           access_scope = {
+#             type       = "cluster"
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
 
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
+# # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+# data "aws_iam_policy" "ebs_csi_policy" {
+#   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+# }
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
+# module "irsa-ebs-csi" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#   version = "5.39.0"
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
+#   create_role                   = true
+#   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+#   provider_url                  = module.eks.oidc_provider
+#   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+#   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+# }
